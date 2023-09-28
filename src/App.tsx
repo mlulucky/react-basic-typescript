ad76be68b86f5de5b78d1e61c38fb1da395ac1c5
mlulucky <dmswjdans@gmail.com>
2023-09-28 22:40:04 +0900

    props 객체로 묶어서 전달

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,29 @@
 // import "./App.css";
 import MyHeader from "./MyHeader";
 import MyFooter from "./MyFooter";
+import Counter from "./Counter";
+import Container from './Container';
 
 function App() {
-	// 인라인 스타일 (객체타입으로)
-	const style =  {
-		App: {
-			backgroundColor: 'black'
-		},
-		h2: {
-			color: '#fff'
-		},
-		bold_text : {
-			color: 'green'
-		}
+	// 컴포넌트에 전달되는 데이터, props 가 많으면 => 객체로 묶어서 전달
+
+	const counterProps = {
+		a: 1,
+		b: "b",
+		c: "c",
+		initialValue: 5
 	}
 
-  let name = "은정";
-	let number = 5;
-	// { } 중괄호 안에 값은 문자열, 숫자만 렌더된다. (boolean 타입, [] 배열은 렌더 안됨)
   return (
-    <div style={style.App}>
-    {/* </div><div className="App"> */}
-      <MyHeader />
-        <h2 style={style.h2}>안녕 리액트 {name}</h2>
-				<b style={style.bold_text} id="bold_text">
-					안녕 타입스크립트 <br/>
-					조건부 렌더링 {number}는 : {number % 2 === 0 ? '짝수' : '홀수'}
-				</b>
-      <MyFooter />
-    </div>
+		<Container>
+			<div>
+				<MyHeader />
+				<Counter {...counterProps}/>
+				{/* <Counter a={1} b={"b"} c={"c"} initialValue = {5}/> */}
+				<MyFooter />
+			</div>
+		</Container>
   );
 }
 
 export default App;
-
-// 최상위 태그 규칙
-// 최상위 태그 하나로 감싸줘야 한다.
-// <></>
-// == <React.Fragment></React.Fragment>
-// React 기능을 import 하지 않는 컴포넌트의 경우는 굳이 React 를 import 하지 않아도 된다.
-
-// 닫힘 태그 <br/> <Image/>
-
-// 조건부 렌더링 : 삼항연산자를 이용해서, 조건이 참, 거짓에 따라서 렌더링 할 수 있다.
-// {조건식 ? 참 : 거짓} 
