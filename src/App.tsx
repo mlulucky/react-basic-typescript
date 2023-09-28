89afa70d6ed8bcf2a83f1bccc2c93dc9ba359f02
mlulucky <dmswjdans@gmail.com>
2023-09-28 23:35:42 +0900

    feat: DiaryEditor 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
+import DiaryEditor from "./DiaryEditor";
 // import "./App.css";
-import MyHeader from "./MyHeader";
-import MyFooter from "./MyFooter";
-import Counter from "./Counter";
-import Container from './Container';
 
 function App() {
-	// 컴포넌트에 전달되는 데이터, props 가 많으면 => 객체로 묶어서 전달
-
-	const counterProps = {
-		a: 1,
-		b: "b",
-		c: "c",
-		initialValue: 5
-	}
 
   return (
-		<Container>
-			<div>
-				<MyHeader />
-				<Counter {...counterProps}/>
-				{/* <Counter a={1} b={"b"} c={"c"} initialValue = {5}/> */}
-				<MyFooter />
-			</div>
-		</Container>
+		<div className="App">
+			<DiaryEditor />
+		</div>
   );
 }
 
