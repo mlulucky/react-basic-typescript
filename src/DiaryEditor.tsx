a5605c167d68b023d2050495f8927e8e6ccfb760
mlulucky <dmswjdans@gmail.com>
2023-09-30 14:20:17 +0900

    refactor: state 를 객체로 묶어서 전달 및 상태변경

--- src/DiaryEditor.tsx
+++ src/DiaryEditor.tsx
@@ -1,29 +1,39 @@
 import { useState } from "react";
 
 const DiaryEditor = () => {
-	// state 를 이용하는 방법이 동일하다 => 하나의 state 로 묶을 수 있다.
-	const [author, setAuthor] = useState("");
-	const [content, setContent] = useState("");
+	// state 를 이용하는 방법이 동일하다 => 하나의 state 로 묶을 수 있다. => 객체로 전달
+	const [state, setState] = useState({
+		author: "", 
+		content: ""
+	});
 
 	return (
 		<div className="DiaryEditor">
 			<h2>오늘의 일기</h2>
 			{/* 작성자 입력 */}
 			<div>
-				<input name="author" value={author} onChange={ // e.target : 이벤트가 발생한 태그
+				<input name="author" value={state.author} onChange={ // e.target : 이벤트가 발생한 태그
 					(e)=> { // onChange 의 event 객체를 매개변수로 받는다.
 						console.log(e.target.value);
-						setAuthor(e.target.value);
+						setState({ // state 객체 값 변경 // 객체의 값을 변경하려면 새로운 객체의 값을 전달해야함 // 🍒 setState(새로운 객체)
+							...state, // 기존 state 값(state.author, state.content)을 전달
+							author: e.target.value, // 새로 입력한 값으로 업데이트 // => 업데이트할 값을 나중에 적는다.(값 업데이트)
+							// content: state.content // 기존 state 값을 전달
+						})
 					}
 				}/>
 			</div>
 			
 			{/* 일기본문 */}
 			<div>
-				<textarea name="content" value={content} onChange={
+				<textarea name="content" value={state.content} onChange={
 					(e) => {
 						console.log(e.target.value);
-						setContent(e.target.value);
+						setState({
+							...state,
+							// author: state.author,
+							content: e.target.value
+						})
 					}
 				}/>
 			</div>
